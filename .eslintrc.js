fe6aa1cc72db5f68095a1bf1b53b630750bd5f91
MonikaMsk <31183294+MonikaMsk@users.noreply.github.com>
2024-01-10 21:54:49 +0100

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,72 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+    "root": true,
+    "parser": '@typescript-eslint/parser',
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "react-native/react-native":true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended",
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-native",
+        ["import"]
+    ],
+    "rules": {
+         // turn on errors for missing imports
+    "import/no-unresolved": "error"
+    },
+     "settings": {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        "project": "path/to/folder",
+
+        // Multiple tsconfigs (Useful for monorepos)
+
+        // use a glob pattern
+        "project": "packages/*/tsconfig.json",
+
+        // use an array
+        "project": [
+          "packages/module-a/tsconfig.json",
+          "packages/module-b/tsconfig.json"
+        ],
+
+        // use an array of glob patterns
+        "project": [
+          "packages/*/tsconfig.json",
+          "other-packages/*/tsconfig.json"
+        ]
+      }
+    }
+  }
+}
+
